867b9c060fbf33ce4907594ce778c68a977d087a
vijayanandrckz <vijayanand.puppala@gmail.com>
2022-01-08 16:49:42 +0530

    changes in tec branch

---
+++ src/test/java/dspdemo/T2.java
@@ -0,0 +1,29 @@
+package dspdemo;
+
+import com.crm.qa.utilities.TestUtils;
+import dsp.pageobjects.Thomepage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class T2 {
+
+
+    //test commit for dsp
+    @Test
+    public void FlightTest(){
+
+        System.setProperty("webdriver.chrome.driver", TestUtils.WORKSAPCE_PATH + "//drivers//chromedriver.exe");
+     //   chromeOptions = new ChromeOptions();
+        //chromeOptions.addArguments("--start-maximized");
+        WebDriver driver = new ChromeDriver();
+        Thomepage thomepage = new Thomepage(driver);
+
+        thomepage.goTo();
+
+        thomepage.getFooterBar().selectFlight();
+        thomepage.getFooterBar().getLinks();
+        thomepage.getHeaderBar().selectFlight();
+        thomepage.getHeaderBar().getLinks();
+    }
+}
